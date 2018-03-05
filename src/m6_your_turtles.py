77fa74e2ecdca0ed2dae4b4253afc6c59ae564b2
antenorm <antenorm@rose-hulman.edu>
2018-03-05 11:30:55 -0500

    m6

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Ryan Antenore.
 """
 ###############################################################################
-# TODO: 1.
+# Done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ###############################################################################
 
@@ -27,3 +27,58 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+blue_turtle = rg.SimpleTurtle('turtle')
+blue_turtle.pen = rg.Pen('midnight blue', 3)
+blue_turtle.speed = 1000  # Fast
+
+# The first square will be 300 x 300 pixels:
+
+size = 40
+sides = 20
+# Do the indented code 13 times.  Each time draws a square.
+for k in range(15):
+
+    # Put the pen down, then draw a square of the given size:
+    blue_turtle.draw_regular_polygon(sides, size)
+
+    # Move a little below and to the right of where the previous
+    # square started.  Do this with the pen up (so nothing is drawn).
+    blue_turtle.pen_up()
+    blue_turtle.right(45)
+    blue_turtle.forward(10)
+    blue_turtle.left(45)
+
+    # Put the pen down again (so drawing resumes).
+    # Make the size for the NEXT square be 12 pixels smaller.
+    blue_turtle.pen_down()
+    size = size - 2
+    sides = sides - 1
+
+red_turtle = rg.SimpleTurtle('turtle')
+red_turtle.pen = rg.Pen('red', 3)
+red_turtle.speed = 40
+
+radius = 20
+red_turtle.pen_up()
+red_turtle.right(45)
+red_turtle.forward(175)
+red_turtle.left(45)
+
+for x in range(15):
+
+    red_turtle.draw_circle(radius)
+
+    red_turtle.pen_up()
+    red_turtle.left(45)
+    red_turtle.forward(10)
+    red_turtle.left(45)
+
+    red_turtle.pen_down()
+    radius = radius + 5
+
+
+window.close_on_mouse_click()
